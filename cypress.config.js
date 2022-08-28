b73645f6165e075c41ce690987769ce1e1ec9cf9
Rk <radhakrishna.nakireddy@gmail.com>
2022-08-29 00:40:22 +0100

    Changes to be committed: 	modified:   .cypress-cucumber-preprocessorrc.json 	new file:   .dockerignore 	new file:   Dockerfile.ci 	new file:   cucumber-json-formatter 	modified:   cypress.config.js 	new file:   cypress/e2e/features/file2.feature 	new file:   cypress/e2e/features/file3.feature 	new file:   cypress/e2e/features/file4.wip.feature 	new file:   docker-compose.yml 	new file:   html-report.js 	modified:   jsonlogs/messages.ndjson 	modified:   package-lock.json 	modified:   package.json

--- cypress.config.js
+++ cypress.config.js
@@ -30,6 +30,7 @@ module.exports = defineConfig({
       return config;
     },
     specPattern: "cypress/e2e/features/*.feature",
+    excludeSpecPattern: "cypress/e2e/features/*.wip.feature",
     baseUrl: "https://www.saucedemo.com",
     chromeWebSecurity: false,
   },
